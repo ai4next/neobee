97085a272865b08720142d751357aad3fabf1b0c
cv70 <2548604505@qq.com>
2026-05-14 11:52:02 +0800

    feat: wire debate_insight and idea_factory nodes into graph

--- neobee/pipeline/nodes/__init__.py
+++ neobee/pipeline/nodes/__init__.py
@@ -1,13 +1,13 @@
 from neobee.pipeline.nodes.cross_review import cross_review_node
+from neobee.pipeline.nodes.debate_insight import debate_insight_node
 from neobee.pipeline.nodes.deep_research import deep_research_node
 from neobee.pipeline.nodes.expert_creation import expert_creation_node
-from neobee.pipeline.nodes.idea_synthesis import idea_synthesis_node
-from neobee.pipeline.nodes.insight_refinement import insight_refinement_node
+from neobee.pipeline.nodes.idea_factory import idea_factory_node
 
 __all__ = [
     "deep_research_node",
     "expert_creation_node",
-    "insight_refinement_node",
+    "debate_insight_node",
     "cross_review_node",
-    "idea_synthesis_node",
+    "idea_factory_node",
 ]
